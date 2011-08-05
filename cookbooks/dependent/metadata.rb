d0df9429a8c2de4e39baafde4e49a51ddb18f4f0
Graham Hughes <graham@rightscale.com>
2011-08-05 16:20:30 -0700

    Initial commit

---
+++ cookbooks/dependent/metadata.rb
@@ -0,0 +1,7 @@
+maintainer "Graham Hughes"
+maintainer_email "graham@rightscale.com"
+license "MIT"
+description "Test some difficulties we were having"
+version "0.1"
+
+recipe "dependent", "Dependent thing"
